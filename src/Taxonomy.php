f2fb949fc7cbcd2207b43b8bd23ac6ef8a5484fd
Roy Sivan <roy@roysivan.com>
2018-02-13 21:11:33 -0800

    fixing conditional

--- src/Taxonomy.php
+++ src/Taxonomy.php
@@ -4,7 +4,7 @@ namespace Lean;
 
 /**
  * Class Taxonomy
- * 
+ *
  * @package Lean
  */
 class Taxonomy {
@@ -97,7 +97,7 @@ class Taxonomy {
 	 * @since 0.1.0
 	 */
 	public function init() {
-		if ( ! post_type_exists( $this->name ) ) {
+		if ( ! taxonomy_exists( $this->name ) ) {
 			if ( empty( $this->objects ) ) {
 				return new \WP_Error( 'missing objects', __( 'You are missing objects to associate the taxonomy to', 'lean' ) );
 			}
